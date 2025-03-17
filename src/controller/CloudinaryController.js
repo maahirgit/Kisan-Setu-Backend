11c82ef01d09181dc4c944c2b7bd1bfc3fad70c4
maahirgit <maahirgit@gmail.com>
2025-03-17 22:37:56 +0530

    latest

--- src/controller/CloudinaryController.js
+++ src/controller/CloudinaryController.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2
 
 const uploadFileinCloudinary = async (file) => {
     cloudinary.config({
-        cloud_name: "dnklrceyc",
-    api_key: "687117512481165",
-    api_secret: "BybWuJQqGr_nuXpfHYEi_VTHcJE",
+        cloud_name: "dcgwuwjj0",
+    api_key: "511297311326428",
+    api_secret: "0gZeRBZ7b5Wqlzs00rWvvGLXH24",
     });
 
     const response = await cloudinary.uploader.upload(file.path)
